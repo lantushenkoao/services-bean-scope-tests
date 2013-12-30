4244006bcab961a5e472647f40053fcad02d25a9
lantushenkoao <lantushenkoao@gmail.com>
2013-12-30 19:58:36 +0200

    Initial check out

---
+++ src/main/java/com/lantushenko/experimental/services_scope/App.java
@@ -0,0 +1,13 @@
+package com.lantushenko.experimental.services_scope;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
